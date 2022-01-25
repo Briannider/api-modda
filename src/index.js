c334b9e48aa4af2c4312dbbf5878b2d9c19e5871
Briannider <braianniderhaus@gmail.com>
2022-01-25 17:50:24 -0300

    api changed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('./db/mongoose');
 const express = require('express');
 const app = express();
@@ -34,40 +33,3 @@ app.post('/product', (req, res) => {
 app.listen(port, () => {
     console.log(`Funcionando en http://localhost:${port}`);
 });
-=======
-require('./db/mongoose');
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
-const Product = require('./model/products');
-
-app.use(express.json());
-
-// Read-- Mostrar todos los productos
-app.get('/products', (req, res) => {
-    Product.find()
-        .then((result) => {
-            res.set('Access-Control-Allow-Origin', '*');
-            res.send(result)
-        })
-        .catch(err => res.status(404).send(err));
-})
-
-// Create -- Agregar productos
-app.post('/product', (req, res) => {
-    const product = new Product(req.body)
-    product.save()
-        .then(() => {
-            res.status(201).send(product);
-        })
-        .catch((err) => {
-            res.status(400).send(err);
-        });
-});
-
-//  Delete -- Borrar un producto
-
-app.listen(port, () => {
-    console.log(`Funcionando en http://localhost:${port}`);
-});
->>>>>>> 8c411f8eac3da0143b46874a396a2b61748cd6ae
